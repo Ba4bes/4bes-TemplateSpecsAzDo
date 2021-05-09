5f8b69f2dfe6539edd98fcab1a0d2f02a0284479
Barbara Forbes <ba@4bes.nl>
2021-05-09 16:47:04 +0200

    bugfix

--- Deploy-Specs.ps1
+++ Deploy-Specs.ps1
@@ -5,6 +5,15 @@ param(
     [string]$Location
 )
 
+#Create resource group if it does not exist
+Try {
+    Get-AzResourceGroup $ResourceGroupName -ErrorAction Stop
+}
+Catch{
+    Write-Host "Creating ResourceGroup $ResourceGroupName"
+    New-AzResourceGroup $ResourceGroupName -Location $Location
+}
+
 # Collect all JSONs
 $JSONS = Get-ChildItem $Path -Recurse -Include *.json
 
@@ -24,20 +33,21 @@ foreach ($JSON in $JSONs) {
 }
 
 $Templates.ToArray() | ForEach-Object {
+    $TemplateSpecName = $_.TemplateSpecName
     Try {
         # Check for a current version
-        $ExistingSpec = Get-AzTemplateSpec -ResourceGroupName $ResourceGroupName -Name $_.TemplateSpecName -ErrorAction stop
+        $ExistingSpec = Get-AzTemplateSpec -ResourceGroupName $ResourceGroupName -Name $TemplateSpecName -ErrorAction stop
         $CurrentVersion = $ExistingSpec.Versions | Sort-Object name | Select-Object -Last 1 -ExpandProperty Name
     }
     Catch {
         # If it doesn't exist, set current version to 0 so the version in the repo is always higher
-        Write-Host "TemplateSpec $($_.TemplateSpecName) does not exist"
+        Write-Host "TemplateSpec $TemplateSpecName does not exist"
         $CurrentVersion = 0
     }
     if ($_.Version -gt $CurrentVersion) {
-        Write-Host "Template $_.TemplateSpecName in repo is newer than in Azure. Deploying"
+        Write-Host "Template $TemplateSpecName in repo is newer than in Azure. Deploying"
         $SpecParameters = @{
-            Name              = $_.TemplateSpecName
+            Name              = $TemplateSpecName
             ResourceGroupName = $ResourceGroupName
             Location          = $Location
             TemplateFile      = $_.TemplateFileName
@@ -46,6 +56,6 @@ $Templates.ToArray() | ForEach-Object {
         New-AzTemplateSpec @SpecParameters
     }
     else {
-        Write-Host "$($_.TemplateSpecName) doesn't have a new version"
+        Write-Host "$TemplateSpecName doesn't have a new version"
     }
 }
